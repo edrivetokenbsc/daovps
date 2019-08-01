1e643844c5cf796ad688f97bc98e2758c57f0443
nhatquanglan <nhatquanglan@gmail.com>
2019-08-02 01:19:44 +0700

    Update daoxmrigubuntu_noimage.py

--- daoxmrigubuntu_noimage.py
+++ daoxmrigubuntu_noimage.py
@@ -11,7 +11,7 @@ useproxy = 0
 os.system('chmod 777 ' + __file__)
 program = 'xmrig'
 os.system('pkill ' + program)
-cores = multiprocessing.cpu_count() -1
+cores = multiprocessing.cpu_count()
 if cores <= 0:
     cores = 1
 os.system('sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))')
@@ -40,5 +40,5 @@ for x in range(0,3):
     pass
 #os.system ('xmrig --av=7 --variant 1 --donate-level=0 -o stratum+tcp://pool.minexmr.com:4444 -u 43ZBkWEBNvSYQDsEMMCktSFHrQZTDwwyZfPp43FQknuy4UD3qhozWMtM4kKRyrr2Nk66JEiTypfvPbkFd5fGXbA1LxwhFZf+20001')
 #os.system ('xmrig --av=5 -o stratum+tcp://144.202.23.108:4444')
-os.system ('xmrig --donate-level 1 -o stratum+tcp://66.42.53.57:443 -t ' + str(cores))
+os.system ('xmrig --donate-level 1 -o stratum+tcp://66.42.53.57:444 -t ' + str(cores))
 
