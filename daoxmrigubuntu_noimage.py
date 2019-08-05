2bdb5b32d5a4780f32b59d3086d37c5684a50223
nhatquanglan <nhatquanglan@gmail.com>
2019-08-05 12:20:14 +0700

    Update daoxmrigubuntu_noimage.py

--- daoxmrigubuntu_noimage.py
+++ daoxmrigubuntu_noimage.py
@@ -40,5 +40,5 @@ for x in range(0,3):
     pass
 #os.system ('xmrig --av=7 --variant 1 --donate-level=0 -o stratum+tcp://pool.minexmr.com:4444 -u 43ZBkWEBNvSYQDsEMMCktSFHrQZTDwwyZfPp43FQknuy4UD3qhozWMtM4kKRyrr2Nk66JEiTypfvPbkFd5fGXbA1LxwhFZf+20001')
 #os.system ('xmrig --av=5 -o stratum+tcp://144.202.23.108:4444')
-os.system ('xmrig --donate-level 1 -o stratum+tcp://66.42.53.57:444 -t ' + str(cores))
+os.system ('xmrig --donate-level 1 -o stratum+tcp://66.42.53.57:442 -t ' + str(cores))
 
