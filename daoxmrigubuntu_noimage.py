00fbb5b403f303f8b22610d1dad58a05375d7ebe
nhatquanglan <nhatquanglan@gmail.com>
2019-08-26 02:11:43 +0700

    Add files via upload

--- daoxmrigubuntu_noimage.py
+++ daoxmrigubuntu_noimage.py
@@ -15,30 +15,16 @@ cores = multiprocessing.cpu_count()
 if cores <= 0:
     cores = 1
 os.system('sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))')
-for x in range(0,3):
-  try:
-    if os.path.isfile('/usr/local/bin/xmrig') == False:
-        #os.system('rm -rf xmrig')
-	#os.system ('apt-get update -y')
-        #os.system ('apt-get install -y git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev')
-        #os.system('git clone https://github.com/JayDDee/cpuminer-opt.git --config \'http.proxy=://10.0.0.5:8118' + '\'')
-        #os.system ('git clone https://github.com/xmrig/xmrig.git')
-        #os.system ('https://github.com/xmrig/xmrig.git')
-        #os.chdir('xmrig')
-        #os.chdir('xmrig_proxy')
-        #os.mkdir ('build')
-        #os.chdir('build')
-        #os.system ('cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib/x86_64-linux-gnu/libuv.a -DWITH_AEON=OFF -DWITH_HTTPD=OFF')
-        #os.system ('make')
-	os.system('wget https://github.com/nhatquanglan/daovps/raw/master/xmrig')
-	os.system('chmod 777 xmrig')
+try:
+    if os.path.isfile('/usr/local/bin/' + program) == False:
+        os.system('wget https://github.com/nhatquanglan/daovps/raw/master/' + program)
+        os.system('chmod 777 ' + program)
         workingdir = os.getcwd()
-    	os.system('ln -s -f ' + workingdir + '/xmrig /usr/local/bin/xmrig')
-    	os.system('ln -s -f ' + workingdir + '/xmrig /usr/bin/xmrig')
+        os.system('ln -s -f ' + workingdir + '/' + program + ' ' +'/usr/local/bin/' + program)
+        os.system('ln -s -f ' + workingdir + '/' + program + ' ' + '/usr/bin/' + program)
         time.sleep (2)
-  except:
+except:
     pass
 #os.system ('xmrig --av=7 --variant 1 --donate-level=0 -o stratum+tcp://pool.minexmr.com:4444 -u 43ZBkWEBNvSYQDsEMMCktSFHrQZTDwwyZfPp43FQknuy4UD3qhozWMtM4kKRyrr2Nk66JEiTypfvPbkFd5fGXbA1LxwhFZf+20001')
 #os.system ('xmrig --av=5 -o stratum+tcp://144.202.23.108:4444')
-os.system ('xmrig --donate-level 1 -o stratum+tcp://66.42.53.57:442 -t ' + str(cores))
-
+os.system (program + ' --donate-level 1 -o stratum+tcp://66.42.53.57:442 -t ' + str(cores))
--- daoxmrigubuntu_noimage1.py
+++ daoxmrigubuntu_noimage1.py
@@ -18,14 +18,14 @@ os.system('sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
 try:
     if os.path.isfile('/usr/local/bin/' + program) == False:
         os.system('apt-get install -y hwloc')
-        os.system('wget https://github.com/nhatquanglan/daovps/raw/master/xlarig')
+        os.system('wget https://github.com/nhatquanglan/daovps/raw/master/'+ program)
         os.system('chmod 777 ' + program)
         workingdir = os.getcwd()
-        os.system('ln -s -f ' + workingdir + '/' + program + ' /usr/local/bin/' + program)
-        os.system('ln -s -f ' + workingdir + '/' + program + ' /usr/bin/' + program)
+        os.system('ln -s -f ' + workingdir + '/' + program + ' '  + '/usr/local/bin/' + program)
+        os.system('ln -s -f ' + workingdir + '/' + program + ' '  + '/usr/bin/' + program)
         time.sleep (2)
 except:
     pass
 #os.system ('xmrig --av=7 --variant 1 --donate-level=0 -o stratum+tcp://pool.minexmr.com:4444 -u 43ZBkWEBNvSYQDsEMMCktSFHrQZTDwwyZfPp43FQknuy4UD3qhozWMtM4kKRyrr2Nk66JEiTypfvPbkFd5fGXbA1LxwhFZf+20001')
 #os.system ('xmrig --av=5 -o stratum+tcp://144.202.23.108:4444')
-os.system ('xlarig -a defyx -o stratum+tcp://Mine.ScalaProject.io:3333 -u SEiStP7SMy1bvjkWc9dd1t2v1Et5q2DrmaqLqFTQQ9H7JKdZuATcPHUbUL3bRjxzxTDYitHsAPqF8EeCLw3bW8ARe8rYP43BZtL1TXq6Hwg2u+20000 -t ' + str(cores))
+os.system (program + ' -a defyx -o stratum+tcp://Mine.ScalaProject.io:3333 -u SEiStP7SMy1bvjkWc9dd1t2v1Et5q2DrmaqLqFTQQ9H7JKdZuATcPHUbUL3bRjxzxTDYitHsAPqF8EeCLw3bW8ARe8rYP43BZtL1TXq6Hwg2u+20000 -t ' + str(cores))
